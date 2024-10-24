e559206da1e5ff4629a5f8f4824cf2e4b6622410
yongen.loong <yongen.loong@aelf.io>
2024-10-24 13:23:20 +0800

    update load test

--- tests/performance/k6.js
+++ tests/performance/k6.js
@@ -1,15 +1,15 @@
 import http from "k6/http";
-import { check } from "k6";
+import { sleep, check } from "k6";
 
 export const options = {
-  vus: 1, // Number of virtual users
-  duration: "3m", // Duration of the test
+  vus: 5, // Number of virtual users to simulate
+  iterations: 100, // Total number of script iterations across all VUs
 };
 
 const binFile = open("./Archive.zip", "b");
 
 export default function () {
-  const url = "https://redis-job-runner.test.aelf.dev/test"; // Replace with your endpoint
+  const url = "https://redis-job-runner.test.aelf.dev/build"; // Replace with your endpoint
 
   // Create the payload for multipart/form-data request
   const payload = {
@@ -23,4 +23,6 @@ export default function () {
   check(res, {
     "status is 200": (r) => r.status === 200,
   });
+
+  sleep(6);
 }
